cd82414d2fe61bf23aa3da6578a1b358c4b4d070
Roberto Achacoso <bertoa@gmail.com>
2014-06-18 17:33:11 -0700

    add friend message display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,9 @@ class HomeController < ApplicationController
   
 	def front
 
-		if session[:user_id]
-		
-			user = User.where(id: session[:user_id]).first
+		if @current_user
 
+			user = @current_user
 			@want_to_be_their_friend = []
 			@wants_to_be_my_friend = []
 			@myfriends = []
@@ -13,19 +12,20 @@ class HomeController < ApplicationController
 
 
 			# get people i requested
-			myrequests = user.friends.i_asked.pluck(:friend_uid)
-			@want_to_be_their_friend = User.in(_id: myrequests)
-			@inqueue.concat myrequests
+			myrequests_ids = user.friends.i_asked.pluck(:friend_uid)
+			@myrequests = user.friends.i_asked.only(:friend_uid, :initial_request_message)
+			@want_to_be_their_friend = User.in(_id: myrequests_ids)
+			@inqueue.concat myrequests_ids
 			
 			# get people who requested me
-			requestme = user.friends.who_asked_me.pluck(:friend_uid)
-			@wants_to_be_my_friend= User.in(_id: requestme)
-			@inqueue.concat requestme
+			requestme_ids = user.friends.who_asked_me.pluck(:friend_uid)
+			@wants_to_be_my_friend= User.in(_id: requestme_ids)
+			@inqueue.concat requestme_ids
 
 			# get friends who accepted
-			my_friends = user.friends.accepted.pluck(:friend_uid)
-			@myfriends = User.in(_id: my_friends)
-			@inqueue.concat my_friends
+			my_friends_ids = user.friends.accepted.pluck(:friend_uid)
+			@myfriends = User.in(_id: my_friends_ids)
+			@inqueue.concat my_friends_ids
 
 			# get all others
 			@allpeople = User.not_in(:_id => @inqueue).order_by(:lastname.asc)
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,6 @@
 class UserController < ApplicationController
   
 	def new
-
 	end
 
 	def create
@@ -14,6 +13,22 @@ class UserController < ApplicationController
 		user.save!
 		session[:user_id] = user.id.to_s
 		redirect_to '/'
+
+	end
+
+	def update
+	end
+
+	def do_update
+
+		user = @current_user
+
+		user.firstname = params[:user][:firstname]
+		user.lastname = params[:user][:lastname]
+		user.email = params[:user][:email]
+		user.save!
+
+		redirect_to '/preferences/edituser', :flash => { :userupdate => "Your have successfully updated your profile!" }
 
 	end
 
